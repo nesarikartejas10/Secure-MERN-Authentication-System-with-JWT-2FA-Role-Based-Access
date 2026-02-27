797400f637c0ed011d0d4a33e54cb608274cb34c
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-02-27 06:39:01 +0530

    create User model with role-based access and authentication fields

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    maxlength: 3,
+  },
+  password: {
+    type: String,
+    required: true,
+    maxlength: 8,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+});
+
+export const User = mongoose.model("User", userSchema);

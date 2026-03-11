ee7abd7d9771840fadc300991429f7253543fc10
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-11 10:02:27 +0530

    integrate AuthLayout with protected and public route configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./pages/auth/NotFound";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import VerifyOtp from "./pages/auth/VerifyOtp";
+import Verify from "./pages/auth/Verify";
+import Home from "./pages/Home";
+
 const App = () => {
-  return <div>App</div>;
+  const isAuth = true;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={isAuth ? <Home /> : <Login />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/verifyOtp" element={<VerifyOtp />} />
+          <Route path="/token/:token" element={<Verify />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Welcome Secure Auth system</div>;
+};
+
+export default Home;

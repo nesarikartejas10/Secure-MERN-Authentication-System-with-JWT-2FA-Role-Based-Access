3ef99c8141a5d031be00bdf60ccf375252fb2322
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-12 06:45:52 +0530

    add user login page with form UI and navigation

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import { FaTableCellsRowUnlock } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import Label from "../../components/ui/Label";
+import Input from "../../components/ui/Input";
+import Button from "../../components/ui/Button";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className="max-w-110 w-full m-2 p-10 border border-gray-300 shadow-xl bg-white shadow-gray-300/50 rounded-xl">
+        <FaTableCellsRowUnlock className="text-5xl text-center w-full" />
+        <h1 className="text-[28px] text-center font-bold mt-2">
+          Login to Your Account
+        </h1>
+        <p className="text-base text-gray-600 mx-auto mt-2 text-center max-w-72">
+          Enter your credentials to access your dashboard
+        </p>
+        <form>
+          <div className="w-full mt-4">
+            <Label
+              htmlFor="email"
+              className="text-sm text-black font-medium mb-1.5 block"
+              value="Email"
+            />
+
+            <Input
+              type="email"
+              placeholder="Email"
+              id="email"
+              className="w-full border border-gray-400 text-base px-3 py-2 leading-normal rounded-lg bg-white"
+            />
+          </div>
+
+          <div className="w-full mt-4">
+            <div className="flex items-center justify-between">
+              <Label
+                htmlFor="password"
+                className="text-sm text-black font-medium mb-1.5 block"
+                value="Password"
+              />
+              <p className="text-xs font-semibold mb-1.5 text-blue-600">
+                Forgot Password?
+              </p>
+            </div>
+
+            <Input
+              type="password"
+              placeholder="Password"
+              id="password"
+              className="w-full border border-gray-400 text-base px-3 py-2 leading-normal rounded-lg bg-white"
+            />
+          </div>
+
+          <div className="flex items-center gap-1.5 mt-2">
+            <Input type="checkbox" className="scale-110" />
+            <span className="text-sm text-gray-500">Remember Me</span>
+          </div>
+
+          <div className="w-full mt-5">
+            <Button
+              type="submit"
+              className="w-full text-center shadow-lg shadow-blue-600/50 py-2.5 bg-blue-600 cursor-pointer text-white font-bold rounded-lg text-base transition-all duration-150 ease-in hover:bg-blue-700 active:scale-[0.98]"
+              title="Sign In"
+            />
+          </div>
+
+          <p className="text-sm text-gray-600 text-center mt-4">
+            Don't have an account?{" "}
+            <Link className="text-blue-600 underline">Register</Link>
+          </p>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Login;

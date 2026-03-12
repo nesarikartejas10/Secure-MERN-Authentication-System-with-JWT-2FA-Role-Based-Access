503928bffacc762364d6bb63770e348da9fd389d
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-12 19:37:55 +0530

    feat: prevent duplicate users and hash password with bcrypt

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,6 +1,8 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import createHttpError from "http-errors";
 import redisClient from "../config/redis.js";
+import { User } from "../routes/auth.routes.js";
+import bcrypt from "bcryptjs";
 
 export const registerUser = asyncHandler(async (req, res, next) => {
   const { name, email, password } = req.validatedData;
@@ -9,12 +11,23 @@ export const registerUser = asyncHandler(async (req, res, next) => {
     return next(createHttpError(400, "All fields are required"));
   }
 
+  //ip+email rate limiting
   const rateLimitKey = `register-rate-limit:${req.ip}:${email}`;
   if (await redisClient.get(rateLimitKey)) {
     return next(
       createHttpError(429, "Too many requets. Please try again later"),
     );
   }
+
+  //check existing user in database
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return next(createHttpError(400, "User already exists"));
+  }
+
+  //password hashing
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   return res.status(201).json({
     success: true,
     message: "User registered successfully",

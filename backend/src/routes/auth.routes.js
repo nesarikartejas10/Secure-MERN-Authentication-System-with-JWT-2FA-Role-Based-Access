ecb55ff34b1c940484b59ecab6f5dec253996074
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-12 19:22:01 +0530

    add POST /register route with validation middleware

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { registerUser } from "../controllers/auth.controllers.js";
+import { registerUserSchema } from "../validators/user.validation.js";
+import validate from "../middlewares/validate.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register", validate(registerUserSchema), registerUser);
 
 export default router;

44e8527e5737c123d0a69e767a292eb48468a333
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-12 12:11:22 +0530

    add timestamps for created and updated field

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,30 +1,34 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 3,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
 
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
 
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-});
+  { timestamps: true },
+);
 
 export const User = mongoose.model("User", userSchema);

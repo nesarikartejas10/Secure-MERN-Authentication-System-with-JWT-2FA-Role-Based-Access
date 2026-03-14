b890106594cc791452e3da46c97acfa5d038b687
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-03-14 19:07:11 +0530

    store refresh token in redis

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -1,7 +1,8 @@
 import jwt from "jsonebtoken";
 import { config } from "../config/envConfig.js";
+import redisClient from "../config/redis.js";
 
-const generateToken = (id, res) => {
+const generateToken = async (id, res) => {
   const accessToken = jwt.sign({ id }, config.accessJwtSecret, {
     exppresIn: "1m",
   });
@@ -9,4 +10,7 @@ const generateToken = (id, res) => {
   const refreshToken = jwt.sign({ id }, config.refreshJwtSecret, {
     exppresIn: "7d",
   });
+
+  const refreshTokenKey = `refresh_token:${id}`;
+  await redisClient.setEx(refreshTokenKey, 7 * 24 * 60 * 60, refreshToken);
 };
